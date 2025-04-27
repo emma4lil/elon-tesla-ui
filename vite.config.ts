e0b36499d58937a0001e65f981ed201ca90dbe8b
EmmanuelH <emma4lil@gmail.com>
2025-04-27 16:03:50 +0100

    small touches ui alignments

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
     }
   },
   build: {
-    chunkSizeWarningLimit: 1024 * 1024 // Set the limit to 1 MB
+    chunkSizeWarningLimit: 1024 * 1024, // Set the limit to 1 MB
+    target: 'esnext'
   },
   optimizeDeps: {
     exclude: ['vuetify'],
